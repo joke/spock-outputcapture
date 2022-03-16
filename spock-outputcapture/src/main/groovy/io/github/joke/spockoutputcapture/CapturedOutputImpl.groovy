213c2278b4e2e51775248e69d00f2d90cf7f3a4e
Gerlach, Winfried <Winfried.Gerlach@draeger.com>
2022-03-16 10:50:15 +0100

    Consider other line endings

--- spock-outputcapture/src/main/groovy/io/github/joke/spockoutputcapture/CapturedOutputImpl.groovy
+++ spock-outputcapture/src/main/groovy/io/github/joke/spockoutputcapture/CapturedOutputImpl.groovy
@@ -15,7 +15,7 @@ class CapturedOutputImpl implements CapturedOutput {
 
     @Override
     List<String> getLines() {
-        buffer.toString().split('\n')
+        buffer.toString().split('(\r\n|\n|\r)')
     }
 
 }
