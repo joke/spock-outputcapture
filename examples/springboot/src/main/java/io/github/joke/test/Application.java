71f954ed4ea23bc563d52c47e2155b49784030c2
Joke de Buhr <joke@xckk.de>
2022-09-05 12:04:54 +0200

    build: improve test setup
    Signed-off-by: Joke de Buhr <joke@xckk.de>

---
+++ examples/springboot/src/main/java/io/github/joke/test/Application.java
@@ -0,0 +1,14 @@
+package io.github.joke.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+
+    static void main(String[] args) {
+        run(Application.class, args);
+    }
+
+}
