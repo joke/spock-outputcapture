33d519cedd9bc0f79970be97e71e0183af1e5cf9
Joke de Buhr <joke@xckk.de>
2024-03-16 12:00:10 +0100

    fix: jdk dependend string splitting #157
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- spock-outputcapture/src/main/java/io/github/joke/spockoutputcapture/CapturedOutputImpl.java
+++ spock-outputcapture/src/main/java/io/github/joke/spockoutputcapture/CapturedOutputImpl.java
@@ -2,9 +2,8 @@ package io.github.joke.spockoutputcapture;
 
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 
 public class CapturedOutputImpl implements CapturedOutput {
@@ -20,6 +19,9 @@ public class CapturedOutputImpl implements CapturedOutput {
 
     @Override
     public List<String> getLines() {
+        if (buffer.length() == 0) {
+            return emptyList();
+        }
         return NEWLINES.splitAsStream(buffer.toString())
                 .collect(toList());
     }
